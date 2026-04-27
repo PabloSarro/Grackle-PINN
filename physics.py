aaa25cd95e9221e94f8f262efe21c6ed4934af5a
PabloSarro <pablo.sarrosanchez@epfl.ch>
2026-04-27 23:51:06 +0200

    Refined physics loss computation

--- physics.py
+++ physics.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from data_utils import DEBUG_helper
 
 class GrackleRates:
     """Analytical fits for Grackle primordial chemistry and cooling."""
@@ -123,84 +124,146 @@ class PhysicsLossManager:
         self.y_max = y_max
         self.S_y = y_max - y_min
 
-    def get_residuals(self, batch_x_norm, preds_norm):
+    def get_residuals(self, batch_x, preds):
         """
         Computes the residuals for all species.
-        batch_x: [log10_t, log10_T0, log10_HI0, log10_HII0]
+            
+            batch_x: [t, log10_T0, log10_HI0, log10_HII0] (all normalised)
+            preds: [log10_T, log10_HI, log10_HII] (all normalised)
         """
-        if torch.isnan(preds_norm).any():
-            print(f"DEBUG: 'preds_norm' contains NaNs from the model output.")
-
-        # Recover physical (log) values from normalized inputs
-        preds_log = preds_norm * self.S_y + self.y_min
-
-        # Forward pass
-        T_log, HI_log, HII_log = torch.clamp(preds_log[:, 0:1], min=0.0, max=9.0), torch.clamp(preds_log[:, 1:2], min=-20.0, max=5.0), torch.clamp(preds_log[:, 2:3], min=-20.0, max=5.0)
-        # HeI_log, HeII_log, HeIII_log = preds_log[:, 3:4], preds_log[:, 4:5], preds_log[:, 5:6]
-        
-        # Convert to linear scale for physics calculations
-        T = 10**T_log
-        nHI = 10**HI_log
-        nHII = 10**HII_log
-        # nHeI = 10**HeI_log
-        # nHeII = 10**HeII_log
-        # nHeIII = 10**HeIII_log
-        
-        # Derived electron density
-        ne = nHII # + nHeII + 2 * nHeIII
-
-
+        DEBUG_helper("get_residuals: Input batch_x", batch_x)
+        DEBUG_helper("get_residuals: Input preds", preds)
 
         # // ----- COMPUTATION OF PHYSICAL LOSS FUNCTIONS ----- \\
+        # The rate equations are in linear space (dy/dt = ...)
+        # Our t is normalised (t_norm) and y is additionally in log space (log_y_norm)
+        # 
+        # We will rewrite the physical loss equations by using the chain rule:
+            # d(log_y_norm)/dt_norm = d(log_y_norm)/dlog_y * dlog_y/dy *  dy/dt * dt/dt_norm
+            #         [---]         =      [1/cm^3}]       *   [---]   *[cm^3/s]*    [s]
 
-        #       // ----- 1. LHS (dn/dt and dT/dt)) ----- \\
+        #       // ----- 1. LHS: d(log_y_norm)/dt_norm ----- \\
 
-        # Function for d(log10_y)/dt
-        def get_phys_grad(y_log, target_idx):
-            # grad_norm is d(y_log_norm) / d(t_lin_norm)
-            grad_norm = torch.autograd.grad(
-                y_log, batch_x_norm, grad_outputs=torch.ones_like(y_log),
+        def get_LHS(log_y_norm):
+            return torch.autograd.grad(
+                log_y_norm, batch_x,
+                grad_outputs=torch.ones_like(log_y_norm), 
                 create_graph=True, retain_graph=True
             )[0][:, 0:1]
+        
+        T_LHS = get_LHS(preds[:, 0:1]) # d(log_T_norm)/dt_norm
+        HI_LHS = get_LHS(preds[:, 1:2]) # d(log_HI_norm)/dt_norm
+        HII_LHS = get_LHS(preds[:, 2:3]) # d(log_HII_norm)/dt_norm
+        DEBUG_helper("LHS: T_LHS", T_LHS)
+        DEBUG_helper("LHS: HI_LHS", HI_LHS)
+        DEBUG_helper("LHS: HII_LHS", HII_LHS)
 
-            # Chain rule: d(y_log)/dt = (S_y / S_t) * d(y_norm)/d(t_norm)
-            # self.S_x[0] is the scaling factor for Time (t_max - t_min)
-            return (grad_norm * self.S_y[target_idx]) / self.S_x[0]
+        #   // ----- 2. RHS: d(log_y_norm)/dlog_y * dlog_y/dy * dy/dt * dt/dt_norm ----- \\
 
-        # With the previous, we have converted the log derivatives (since PINN 
-        # is in log space) to linear derivatives (for physical loss functions):
-        dlogT_dt = get_phys_grad(T_log, target_idx=0) # dT/dt
-        dlogHI_dt = get_phys_grad(HI_log, target_idx=1) # dnHI/dt
-        dlogHII_dt = get_phys_grad(HII_log, target_idx=2) # dnHII/dt
+        #     // ----- 2.1: d(log_y_norm)/dlog_y ----- \\
 
+        # log_y_norm = (log_y - y_min) / S_y --> d(log_y_norm)/d(log_y) = 1 / S_y
 
-        #       // ----- 2. RHS (k_{ij}*n_i*n_j) ----- \\
-
-        # // --- 2.1. Chemical residuals --- \\
-        rates = self.phys.compute_chemical_rates(T)
-
-        HII_gain = rates['k1']*nHI*ne + rates['k7']*(nHI**2) # Terms contributing to the production of HII (and loss of HI)
-        HI_gain = rates['k2']*nHII*ne # Term contributing to the production of HI (and loss of HII)
-
-        rhs_HI_lin = HI_gain - HII_gain # k7*nHI*nHI - k1*nHI*ne - k2*nHII*ne
-        rhs_HII_lin = HII_gain - HI_gain # k1*nHI*ne + k2*nHII*ne - k7*nHI*nHI
+        T_RHS_1 = 1.0 / self.S_y[0]
+        HI_RHS_1 = 1.0 / self.S_y[1]
+        HII_RHS_1 = 1.0 / self.S_y[2]
+        DEBUG_helper("RHS_1", T_RHS_1)
+        DEBUG_helper("RHS_1", HI_RHS_1)
+        DEBUG_helper("RHS_1", HII_RHS_1)
 
 
-        # // --- 2.2. Thermal (Energy) residuals --- \\
-        lambda_tot = self.phys.compute_cooling_rates(T, nHI, nHII, ne, rates)
-        n_tot = nHI + nHII + ne
+        #    // ---------- 2.2: dlog_y/dy ---------- \\
+
+        # log_y = log10(y) --> dlog_y/dy = 1 / (y * ln(10))
+        
+        # 2.2.1. Denormalise values
+        preds_denorm = preds * self.S_y + self.y_min
+
+        # Clamp to avoid numerical issues in exponential (2.2.2) and division (2.2.3)
+        T_log = torch.clamp(preds_denorm[:, 0:1], min=0.0, max=self.y_max[0]+1.0)
+        HI_log = torch.clamp(preds_denorm[:, 1:2], min=-10.0, max=self.y_max[1]+1.0)
+        HII_log = torch.clamp(preds_denorm[:, 2:3], min=-10.0, max=self.y_max[2]+1.0)
+        # HeI_log, HeII_log, HeIII_log = preds_denorm[:, 3:4], preds_denorm[:, 4:5], preds_denorm[:, 5:6]
+
+        # 2.2.2. Linearise values
+        T = torch.pow(10, T_log)
+        nHI = torch.pow(10, HI_log)
+        nHII = torch.pow(10, HII_log)
+        # nHeI = torch.pow(10, HeI_log)
+        # nHeII = torch.pow(10, HeII_log)
+        # nHeIII = torch.pow(10, HeIII_log)
+        DEBUG_helper("RHS_2: Linear T", T)
+        DEBUG_helper("RHS_2: Linear nHI", nHI)
+        DEBUG_helper("RHS_2: Linear nHII", nHII)
+
+        # 2.2.3. Compute second term in RHS
+        ln10 = np.log(10.0)
+        T_RHS_2 = 1 / (T * ln10)
+        HI_RHS_2  = 1 / (nHI * ln10)
+        HII_RHS_2 = 1 / (nHII * ln10)
+        DEBUG_helper("RHS_2: T_RHS_2", T_RHS_2)
+        DEBUG_helper("RHS_2: HI_RHS_2", HI_RHS_2)
+        DEBUG_helper("RHS_2: HII_RHS_2", HII_RHS_2)
+
+
+        #    // ---------- 2.3: dy/dt ---------- \\
+
+        # This is where our cooling rate and chemical rate equations come in, since now both variables are linear and denormalised.
+
+        # // ----- 2.3.1: dT/dt ----- \\
         
         # E = rho · e (E: volumetric internal energy)
         # rho = n_{tot}·mu·m_H (rho: density)
         # e = (kT)/((gamma-1)·mu·m_H) (e: specific internal energy)
         # => E = rho · e = n_{tot}·(kT)/(gamma-1)
-        # => dE/dt = n_{tot}·(k/(gamma-1))·dT/dt (assuming n_{tot} is locally constant over the timestep)
+        # => dE/dt = n_{tot}·k/(gamma-1)·dT/dt (assuming n_{tot} is locally constant over the timestep)
         # => dE/dt = -lambda_tot (energy lost due to cooling)
-        # => -lambda_tot = n_{tot}·(k/(gamma-1))·dT/dt => dT/dt = - (gamma-1)·lambda_tot / (n_{tot}·k)
-        gamma = 5.0/3.0
-        # mu = 1.0 // Ignored by now, but Lessandre noted that, since mu(T), the eqn. would need to be solved iteratively.
+        # => -lambda_tot = n_{tot}·k/(gamma-1)·dT/dt => dT/dt = - (gamma-1)·lambda_tot / (n_{tot}·k)
 
-        rhs_T_lin = - (gamma-1) * lambda_tot / (n_tot * self.phys.k_B)
+        ne = nHII # + nHeII + 2 * nHeIII
+        n_tot = nHI + nHII + ne
+        DEBUG_helper("RHS_3: T", T, min_val=0.0, max_val=1e15)
+        rates = self.phys.compute_chemical_rates(T)
+        lambda_tot = self.phys.compute_cooling_rates(T, nHI, nHII, ne, rates)
+        gamma = 5.0/3.0
+        # mu = 1.0 // Not relevant in my analysis, but Lessandre noted that, since mu(T), the eqn. would need to be solved iteratively.
+        DEBUG_helper("RHS_3: lambda_tot", lambda_tot)
+        DEBUG_helper("RHS_3: n_tot", n_tot)
+        T_RHS_3 = - (gamma-1) * lambda_tot / (n_tot * self.phys.k_B)
+        DEBUG_helper("RHS_3: T_RHS_3", T_RHS_3)
+
+
+        # // ----- 2.3.2: dn/dt ----- \\
+
+        # dn/dt = gain(n) - loss(n),
+        # where gain and loss are computed from chemical reaction rates of the form k_{ij}*n_i*n_j
+
+        # 2.3.2.1. Compute the chemical rates.
+        k1 = rates['k1']
+        k2 = rates['k2']
+        k7 = rates['k7']
+
+        # 2.3.2.2. Compute the gain and loss terms.
+        # Terms contributing to the production of HII (and loss of HI)
+        HII_gain = k1*nHI*ne + k7*(nHI**2)
+        HI_loss = HII_gain
+        # Term contributing to the production of HI (and loss of HII)
+        HI_gain = k2*nHII*ne 
+        HII_loss = HI_gain
+
+        # 2.3.2.3. Compute third term in RHS.
+        HI_RHS_3 = HI_gain - HI_loss # k7*nHI*nHI - k1*nHI*ne - k2*nHII*ne
+        HII_RHS_3 = HII_gain - HII_loss # k1*nHI*ne + k2*nHII*ne - k7*nHI*nHI
+
+
+        #       // ----- 2.4: dt/dt_norm ----- \\
+        # t_norm = (t - t_min) / S_x --> t = t_norm·S_x + t_min --> dt/dt_norm = S_x
+        # This must be in seconds, since term 2.3 computes the rates per second (cm^3/s)
+
+        SEC_PER_YEAR = 86400*365.256363004 # Seconds per year
+        RHS_4 = self.S_x[0]*SEC_PER_YEAR
+        DEBUG_helper("RHS_4", RHS_4)
+
 
         # # --- THE TOTAL INSPECTION SYSTEM ---
         # with torch.no_grad():
@@ -226,18 +289,24 @@ class PhysicsLossManager:
         # # --- END INSPECTION ---
 
         
-        # // ----- 3. Log-Space residuals ----- \\
-        # Multiply linear RHS by (t/y) to match the LHS log-derivatives.
-        eps = 1e-8
-        ln10 = np.log(10.0)
+        # // ----- 3. Physical Loss Computation: ((LHS - RHS)/(RHS + eps))^2 ----- \\
+        eps = 0.1
 
-        # Transform RHS: dy_log/dt = (1 / (y_lin * ln10)) * dy_lin/dt
-        rhs_HI_log  = rhs_HI_lin  / (torch.clamp(nHI, min=1e-10) * ln10)
-        rhs_HII_log = rhs_HII_lin / (torch.clamp(nHII, min=1e-10) * ln10)
-        rhs_T_log   = rhs_T_lin   / (torch.clamp(T, min=1.0) * ln10)
+        T_RHS   = T_RHS_1 * T_RHS_2 * T_RHS_3 * RHS_4
+        HI_RHS  = HI_RHS_1 * HI_RHS_2 * HI_RHS_3 * RHS_4
+        HII_RHS = HII_RHS_1 * HII_RHS_2 * HII_RHS_3 * RHS_4
+        DEBUG_helper("Total RHS: T_RHS", T_RHS)
+        DEBUG_helper("Total RHS: HI_RHS", HI_RHS)
+        DEBUG_helper("Total RHS: HII_RHS", HII_RHS)
+        # Previously, denom. with:  / (torch.abs(T_RHS) + eps)
+        loss_T   = torch.mean(((T_LHS - T_RHS) / (torch.abs(T_RHS) + eps))**2) # DEBUG: In the denominator, usually a scale > 0 is preferred (rather than T_RHS).
+        loss_HI  = torch.mean(((HI_LHS - HI_RHS) / (torch.abs(HI_RHS) + eps))**2) # Same for these 2 below.
+        loss_HII = torch.mean(((HII_LHS - HII_RHS) / (torch.abs(HII_RHS) + eps))**2)
+        # print(f"loss_T={loss_T}")
+        # print(f"loss_HI={loss_HI}")
+        # print(f"loss_HII={loss_HII}")
+        DEBUG_helper("Physics Loss: loss_T", loss_T)
+        DEBUG_helper("Physics Loss: loss_HI", loss_HI)
+        DEBUG_helper("Physics Loss: loss_HII", loss_HII)
 
-        loss_HI = torch.mean(((dlogHI_dt - rhs_HI_log) / (torch.abs(rhs_HI_log) + eps))**2)
-        loss_HII = torch.mean(((dlogHII_dt - rhs_HII_log) / (torch.abs(rhs_HII_log) + eps))**2)
-        loss_T = torch.mean(((dlogT_dt - rhs_T_log) / (torch.abs(rhs_T_log) + eps))**2)
-
-        return loss_HI + loss_HII + loss_T
+        return loss_T + loss_HI + loss_HII
